e0157bafbe7e37366ed6e2e299f53bf5bfad3f73
TaeHyoun Kim <kth9903@gmail.com>
2024-03-11 19:31:50 +0900

    배당금 정보 수집 로직 구현

--- src/main/java/com/zerobase/DividendApplication.java
+++ src/main/java/com/zerobase/DividendApplication.java
@@ -10,36 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DividendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DividendApplication.class, args);
-        try {
-            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1709856000&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
-            Document document = connection.get();
+//        SpringApplication.run(DividendApplication.class, args);
+        String s = "Hello my name is %s";
+        String[] names = {"GREEN", "BLUE", "RED"};
 
-            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element ele = eles.get(0);
-
-            Element tbody = ele.children().get(1);
-            for(Element e : tbody.children()){
-                String txt = e.text();
-                if(!txt.endsWith("Dividend")){
-                    continue;
-                }
-
-                String[] splits = txt.split(" ");
-                String month = splits[0];
-                int day = Integer.valueOf(splits[1].replace(",", ""));
-                int year = Integer.valueOf(splits[2]);
-                String dividend = splits[3];
-
-                System.out.println(year +"/"+ month+"/"+day+" -> "+ dividend);
-            }
-        }catch (IOException e){
-            e.printStackTrace();
+        for(String name : names){
+            System.out.println(String.format(s, name));
         }
+        System.out.println(s);
     }
-
 }
